db48834d39a1b73deb04f6bbacd402922b4d161d
Bart Olsthoorn <bartolsthoorn@gmail.com>
2014-04-16 13:07:40 +0200

    Add terminal profile and update README.md

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Leidzsh
 
-Leidzsh is a oh-my-zsh theme of [Jan](https://github.com/jandintel) & [Bart](https://github.com/bartolsthoorn), based in Leiden.
+Dotfiles of [Bart](https://github.com/bartolsthoorn), based in Leiden.
 
-Clone this repo and symlink theme:
-```bash
-ln -s ~/leidzsh/leidzsh.zsh-theme leidzsh.zsh-theme
-```
+Clone this repo to `~` and run `setup.sh`
--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 # Symlink oh-my-zsh theme
-ln -s ~/leidzsh/leidzsh.zsh-theme leidzsh.zsh-theme
+ln -s ~/leidzsh/leidzsh.zsh-theme ~/.oh-my-zsh/themes/leidzsh.zsh-theme
+ln -s ~/leidzsh/.tmuxrc ~/.tmuxrc
 
 # Add .DS_Store to global .gitignore
 echo .DS_Store > ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+# Notify TODOs
+echo "Import SolarizedNight.terminal as Terminal Profile"
